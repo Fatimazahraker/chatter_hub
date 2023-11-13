546d256bcf288f123548c65a51b85e568504f2d7
Fatimazahraker <fatimazahraker>
2023-11-13 22:29:10 +0000

    task

---
+++ wtfform_field.py
@@ -0,0 +1,17 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length, EqualTo, ValidationError
+
+from models import User
+
+
+class RegistrationForm(FlaskForm):
+
+    username = StringField('username', validators=[InputRequired(message="Username required"), Length(min=4, max=25, message="Username must be between 4 and 25 characters")])
+    password = PasswordField('password', validators=[InputRequired(message="Password required"), Length(min=4, max=25, message="Password must be between 4 and 25 characters")])
+    firm_pswd = PasswordField('Retype Password', validators=[InputRequired(message="Password required"), EqualTo('password', message="Passwords must match")])
+
+    def validate_username(self, username):
+        user_object = User.query.filter_by(username=username.data).first()
+        if user_object:
+            raise ValidationError("Username already exists. Select a different username.")
