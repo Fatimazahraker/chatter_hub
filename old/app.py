a70a04897d532cee5fcd55359ae22795c4b11f9e
Fatimazahraker <fatimazahraker>
2023-11-20 08:35:36 +0000

    display error

---
+++ old/app.py
@@ -0,0 +1,35 @@
+import secrets
+import os
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from wtfform_field import*
+from models import db, User
+
+
+db_username = os.environ.get('DB_USERNAME', 'default_username')
+db_password = os.environ.get('DB_PASSWORD', 'default_password')
+db_name = os.environ.get('DB_NAME', 'default_database')
+
+app = Flask(__name__)
+app.secret_key = secrets.token_hex(32)
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{db_username}:{db_password}@localhost/{db_name}'
+
+db.init_app(app)
+
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    reg_form = RegistrationForm()
+
+    if reg_form.validate_on_submit():
+        username = reg_form.username.data
+        password = reg_form.password.data
+
+        user = User(username=username, password=password)
+        db.session.add(user)
+        db.session.commit()
+
+        return "sucess"
+    return render_template("index.html", form=reg_form)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
---
+++ static/script/script1.js
@@ -0,0 +1,40 @@
+// script1.js
+
+$(document).ready(function () {
+    // Function to display error messages
+    function displayError(errorElementId, errorMessage) {
+        var errorList = $('#' + errorElementId);
+        errorList.empty(); // Clear previous error messages
+
+        // Append the new error message
+        errorList.append('<li>' + errorMessage + '</li>');
+    }
+
+    // Function to clear previous error messages
+    function clearErrorMessages() {
+        $('.error-list').empty(); // Clear all error lists
+    }
+
+    // Example validation function
+    function validateForm() {
+        // Clear previous error messages
+        clearErrorMessages();
+
+        // Perform client-side validation
+        var usernameInput = $('#Username');
+        var passwordInput = $('#password');
+        var firmPswdInput = $('#firm-pswrd');
+
+        // Example validation: Check if the username is not empty
+        if (usernameInput.val().trim() === '') {
+            displayError('usernameErrors', 'Username cannot be empty.');
+        }
+
+        // Example validation: Check if the password is not empty
+        if (passwordInput.val().trim() === '') {
+            displayError('passwordErrors', 'Password cannot be empty.');
+        }
+        // ... (rest of the validation logic)
+
+    }
+});
