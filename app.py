a67029d30fed614fbf5c55b9450ccfa5d7ab9523
laila22haz <hazmirlaila316@gmail.com>
2023-11-21 01:17:12 +0100

    update in file app.py and scripte.js

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """ flask application"""
-from flask import Flask, render_template
+from flask import Flask, jsonify, render_template, request, redirect, url_for
 import os
-from models import db, User
+from models import *
+from flask_sqlalchemy import SQLAlchemy
 
 db_username = os.environ.get('DB_USERNAME', 'default_username')
 db_password = os.environ.get('DB_PASSWORD', 'default_password')
@@ -10,14 +11,32 @@ db_name = os.environ.get('DB_NAME', 'default_database')
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{db_username}:{db_password}@localhost/{db_name}'
-db.init_app(app)
-
+db = SQLAlchemy(app)
 
 @app.route("/", methods=['GET', 'POST'])
 def signup():
     "registration page"
-    
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        firm_password = request.form['firm_password']
+        if not (username and password and firm_password and password == firm_password):
+            return jsonify({"error": "Invalid input. Please check your data."}), 400
+
+        exit_user = User.query.filter_by(username=username).first()
+        if exit_user:
+            return jsonify({"error": "Username already taken. Please choose another one."}), 400
+
+        new_user = User(username=username, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('success'))
+        #return jsonify({"error": "User registered successfully"}), 200
     return render_template("signup.html")
+
+@app.route("/success")
+def success():
+    return "User registered successfully!"
 
 if __name__ == "__main__":
     """main function"""
--- static/script/script1.js
+++ static/script/script1.js
@@ -2,6 +2,7 @@
 
 $(document).ready(function () {
     // Function to display error messages
+    var registrationSuccess = false;
     function displayError(errorElementId, errorMessage) {
         var errorList = $('#' + errorElementId);
         errorList.empty(); // Clear previous error messages
@@ -28,21 +29,53 @@ $(document).ready(function () {
         // Example validation: Check if the username is not empty
         if (usernameInput.val().trim() === '') {
             displayError('usernameErrors', 'Username cannot be empty.');
+        }else {
+            // Example validation: Check if the username is between 4 and 25.
+            if (usernameInput.val().length < 4 || usernameInput.val().length > 25) {
+                displayError('usernameErrors', 'Username must be between 4 and 25 characters.');
+            }
         }
 
         // Example validation: Check if the password is not empty
         if (passwordInput.val().trim() === '') {
             displayError('passwordErrors', 'Password cannot be empty.');
+        }else {
+            // Example validation: Check if the password is between 4 and 25.
+            if (passwordInput.val().length < 4 || passwordInput.val().length > 25) {
+                displayError('passwordErrors', 'Password must be between 4 and 25 characters.');
+            }
         }
 
         // Example validation: Check if the passwords match
         if (passwordInput.val() !== firmPswdInput.val()) {
             displayError('firmPswdErrors', 'Passwords do not match.');
         }
+        registrationSuccess = true;
     };
 
     // Attach the validateForm function to the click event of the button
-    $('button').on('click', function () {
+    $('#registrationForm button[type="button"]').on('click', function () {
         validateForm();
+        // check the registrationSuccess flag
+        if (registrationSuccess) {
+            // Make an AJAX request to the Flask server to handle registration
+            $.ajax({
+                type: 'POST',
+                url: '/',
+                data: {
+                    username: $('#Username').val(),
+                    password: $('#password').val(),
+                    firm_password: $('#firm-pswrd').val()
+                },
+                success: function (response) {
+                    console.log('Registration success:', response);
+                    // You can handle the success response here
+                },
+                error: function (error) {
+                    console.error('Registration error:', error);
+                    // You can handle the error response here
+                }
+            });
+        }
     });
 });
