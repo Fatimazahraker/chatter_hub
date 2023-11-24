95f951daad70e4f8b7d29c469e047b1f9648fce6
Fatimazahraker <fatimazahraker>
2023-11-24 16:58:21 +0000

    task

--- old/app.py
+++ old/app.py
@@ -4,7 +4,8 @@ from flask import Flask, flash, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import current_user, login_user, LoginManager
 from wtfform_field import*
-from models import db, User
+from models import *
+from flask_socketio import SocketIO, send
 
 
 db_username = os.environ.get('DB_USERNAME', 'default_username')
@@ -12,10 +13,12 @@ db_password = os.environ.get('DB_PASSWORD', 'default_password')
 db_name = os.environ.get('DB_NAME', 'default_database')
 
 app = Flask(__name__)
+
 app.secret_key = secrets.token_hex(32)
 app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{db_username}:{db_password}@localhost:5432/{db_name}'
 
 db.init_app(app)
+socketio = SocketIO(app)
 login_manager = LoginManager(app)
 
 @login_manager.user_loader
@@ -33,6 +36,7 @@ def index():
         user = User(username=username, password=password)
         db.session.add(user)
         db.session.commit()
+        flash('Registered successfully. Please login.', 'success')
         return redirect(url_for("login"))
     return render_template("index.html", form=reg_form)
 
@@ -43,11 +47,36 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
-        if user is None or not user.password == form.password.data:
-            flash('invalid username or password')
+        if user is None or not user.check_password(form.password.data):
+            flash('invalid username or password', category='error')
             return redirect(url_for("login"))
         login_user(user, remember=form.remember_me.data)
-        return ("success")
+        return redirect(url_for('chat'))
     return render_template('login.html', title='Sign In', form=form)
+
+
+@app.route("/logout", methods=['GET'])
+def logout():
+
+    # Logout user
+    logout_user()
+    flash('You have logged out successfully', 'success')
+    return redirect(url_for('login'))
+
+@app.route("/chat", methods=['GET', 'POST'])
+def chat():
+
+    #if not current_user.is_authenticated:
+        #flash('Please login', 'danger')
+        #return redirect(url_for('login'))
+
+    return render_template("chat.html")
+
+@socketio.on('message')
+def message(data):
+     print(f" {data}")
+     send(data)
+
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)
+    socketio.run(app, debug=True)
